bba3866039320176956f2f48789a7790a640a279
Girdhar2 <vanshikajtwg839@gmail.com>
2022-07-12 03:43:29 +0530

    Updates

---
+++ precache-manifest.0c95f9419ff9c61a49df0ee45b93e50c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621f5ac8efd54cca72e19848b80873c3",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "03e08881ba95b43e318b",
+    "url": "/amazon-clone/static/css/main.78c7c615.chunk.css"
+  },
+  {
+    "revision": "aa7987ed03f7d2f3c2fb",
+    "url": "/amazon-clone/static/js/2.c1817248.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/amazon-clone/static/js/2.c1817248.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03e08881ba95b43e318b",
+    "url": "/amazon-clone/static/js/main.fc480f9a.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  }
+]);
